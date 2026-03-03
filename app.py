839fa42a4aabd2acebb82e8edde981a2a0e4d979
Fdl86 <lebrun.sebastien@gmail.com>
2026-03-03 11:55:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -164,7 +164,7 @@ with col_map:
 # ─── LOG & PROFIL AVEC PALIERS ───
 if len(st.session_state.waypoints) > 1:
     st.markdown("---")
-    curr_t = datetime.datetime.now(datetime.timezone.utc)
+    curr_t = datetime.now(timezone.utc)
     nav_rows, dist_p, alt_p, terr_p = [], [0], [], [st.session_state.waypoints[0]["elev"]]
     alt_p.append(st.session_state.waypoints[0]["elev"])
 
